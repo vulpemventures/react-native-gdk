33b4e72e324a089a7ee802f26d49ec13eefc7e28
Andrea <andrea.lin3110@gmail.com>
2024-10-31 11:04:03 +0100

    Update GDK Prebuilts scripts to `0.72.2`
    Update `npm_scripts/android_gdk.sh` and `npm_scripts/ios_gdk.sh` to fetch GDK `release_0.72.2`

--- npm_scripts/android_gdk.sh
+++ npm_scripts/android_gdk.sh
@@ -12,28 +12,37 @@ fi
 
 # Pre-requisites
 function check_command() {
-    command -v $1 >/dev/null 2>&1 || { echo >&2 "$1 not found, exiting."; exit 1; }
+    command -v "$1" >/dev/null 2>&1 || { echo >&2 "$1 not found, exiting."; exit 1; }
 }
 check_command curl
 check_command gzip
 check_command shasum
 
-VERSION="release_0.71.3"
-TARBALL="gdk-android-jni.tar.gz"
-SHA256=6c379748f218493757cd7b1a49e5a032bd61c254e76928292c03601546b6baae
-
+VERSION="release_0.72.2"
+FOLDERNAME="gdk-${VERSION}"
+TARBALL="${FOLDERNAME}.tar.gz"
+SHA256=a091a5e37f3ce69e1281d28bd2ea5a4898a5d48b6fb8059f412058b5e842b783
+DESTINATION="libgreen_gdk-android"
 
 URL="https://github.com/Blockstream/gdk/releases/download/${VERSION}/${TARBALL}"
 
-rm -rf libgreenaddress-android gdk-android-jni gdk-android-jni.tar.gz
+# Remove previous prebuilts
+rm -rf ${DESTINATION} ${FOLDERNAME} ${TARBALL}
 
+# =================================================================================================
 echo "Fetching gdk prebuilt for android supported archs..."
-
 curl -sL -o ${TARBALL} "${URL}"
 echo "${SHA256}  ${TARBALL}" | shasum -a 256 --check
 tar xf ${TARBALL}
+# =================================================================================================
 
-mkdir libgreenaddress-android;
-yes | cp -rf gdk-android-jni/lib/* libgreenaddress-android/
+# Copy the prebuilts for supported android archs
+# https://developer.android.com/ndk/guides/abis
+mkdir ${DESTINATION}
+cp -rf ${FOLDERNAME}/lib/x86_64 ${DESTINATION}/
+cp -rf ${FOLDERNAME}/lib/x86 ${DESTINATION}/
+cp -rf ${FOLDERNAME}/lib/armeabi-v7a ${DESTINATION}/
+cp -rf ${FOLDERNAME}/lib/arm64-v8a ${DESTINATION}/
 
-rm -rf gdk-android-jni gdk-android-jni.tar.gz
+# Cleanup
+rm -rf ${FOLDERNAME} ${TARBALL}
--- npm_scripts/ios_gdk.sh
+++ npm_scripts/ios_gdk.sh
@@ -12,7 +12,7 @@ fi
 
 # Pre-requisites
 function check_command() {
-    command -v $1 >/dev/null 2>&1 || { echo >&2 "$1 not found, exiting."; exit 1; }
+    command -v "$1" >/dev/null 2>&1 || { echo >&2 "$1 not found, exiting."; exit 1; }
 }
 check_command curl
 check_command gzip
@@ -20,74 +20,67 @@ check_command shasum
 check_command xcodebuild
 check_command lipo
 
-# remove previous installs
-rm -f gdk-iphone.tar.gz gdk-iphone-sim.tar.gz gdk-iphone-sim-x86_64.tar.gz libgreenaddress_full.a
-rm -rf gdk-iphone gdk-iphonesim-arm64 gdk-iphonesim-x86_64 libgreenaddress_full.xcframework
+VERSION="release_0.72.2"
 
-VERSION="release_0.71.3"
-
-echo "Fetching gdk prebuilt for iphone physical devices..."
-
-# The version of gdk to fetch and its sha256 checksum for integrity checking
-DEVICE_SHA256="8c5be13bd8cbfaba6bb0e8bfb10fda89541010e15bfee062e8ed6ce468fc48b5"
-# Setup gdk version and url
-DEVICE_TARBALL="gdk-iphone.tar.gz"
+DEVICE_SHA256="f77e292a0a308cd33771e2b17cac21db76f83cc85bc1b2a681f78a8178f57107"
+DEVICE_FOLDER="gdk-iphone"
+DEVICE_TARBALL="${DEVICE_FOLDER}.tar.gz"
 DEVICE_URL="https://github.com/Blockstream/gdk/releases/download/${VERSION}/${DEVICE_TARBALL}"
+DEVICE_NESTED_FOLDER="arm64-apple-ios13.00"
 
+SIM_SHA256="f3237c142b8d03308e376b54d1de92d865c968b36be959255a1244b97495c644"
+SIM_FOLDER="gdk-iphonesim-arm64"
+SIM_TARBALL="gdk-iphone-sim.tar.gz"
+SIM_URL="https://github.com/Blockstream/gdk/releases/download/${VERSION}/${SIM_TARBALL}"
+SIM_NESTED_FOLDER="arm64-apple-ios13.00"
+
+INTEL_SHA256="5a11fff9dc5c42e5d3287b75859ada27b59f85ed3164c58ebd71db5d5f753686"
+INTEL_FOLDER="gdk-iphonesim-x86_64"
+INTEL_TARBALL="gdk-iphone-sim-x86_64.tar.gz"
+INTEL_URL="https://github.com/Blockstream/gdk/releases/download/${VERSION}/${INTEL_TARBALL}"
+INTEL_NESTED_FOLDER="x86_64-apple-ios13.00"
+
+LIBGREEN="libgreen_gdk_full"
+
+# Remove previous installs
+rm -f ${DEVICE_TARBALL} ${SIM_TARBALL} ${INTEL_TARBALL} ${LIBGREEN}.a
+rm -rf ${DEVICE_FOLDER} ${SIM_FOLDER} ${INTEL_FOLDER} ${LIBGREEN}.xcframework
+
+# =================================================================================================
+# PHYSICAL DEVICE
+echo "Fetching gdk prebuilt for iphone physical devices..."
 curl -sL -o ${DEVICE_TARBALL} "${DEVICE_URL}"
 echo "${DEVICE_SHA256}  ${DEVICE_TARBALL}" | shasum -a 256 --check
 tar xf ${DEVICE_TARBALL}
 
-# move gdk headers (only for the first time) into the cpp main folder
-yes | cp -rf gdk-iphone/include/gdk/* cpp/include
-
-
-# xcodebuild -create-xcframework \
-# -library gdk-iphone/lib/iphoneos/libgreenaddress_full.a \
-# -output iphone/libgreenaddress_full.xcframework
-
-
+# ARM SIMULATOR
 echo "Fetching gdk prebuilt for arm64 simulator devices..."
-
-
-# The version of gdk to fetch and its sha256 checksum for integrity checking
-SIM_SHA256="ca6f71031aba6af807ac12172fc0cd15e7ff4ee3873d77ae46efe1b748f8702c"
-# Setup gdk version and url
-SIM_TARBALL="gdk-iphone-sim.tar.gz"
-SIM_URL="https://github.com/Blockstream/gdk/releases/download/${VERSION}/${SIM_TARBALL}"
-
 curl -sL -o ${SIM_TARBALL} "${SIM_URL}"
 echo "${SIM_SHA256}  ${SIM_TARBALL}" | shasum -a 256 --check
 tar xf ${SIM_TARBALL}
 
-
-
+# INTEL SIMULATOR
 echo "Fetching gdk prebuilt for intel simulator devices..."
-
-
-# The version of gdk to fetch and its sha256 checksum for integrity checking
-INTEL_SHA256="42f5f78d2da0667f291ec43e5c1505cd100967eba812958f406ea9547e1f7242"
-# Setup gdk version and url
-INTEL_TARBALL="gdk-iphone-sim-x86_64.tar.gz"
-INTEL_URL="https://github.com/Blockstream/gdk/releases/download/${VERSION}/${INTEL_TARBALL}"
-
 curl -sL -o ${INTEL_TARBALL} "${INTEL_URL}"
 echo "${INTEL_SHA256}  ${INTEL_TARBALL}" | shasum -a 256 --check
 tar xf ${INTEL_TARBALL}
+# =================================================================================================
 
+# move gdk headers (only for the first time) into the cpp main folder
+cp -rf gdk-iphone/include/gdk/* cpp/include
 
 # pack all simulators binaries into a universal binary using lipo
 lipo -create \
-  gdk-iphonesim-x86_64/lib/iphonesimulator/libgreenaddress_full.a \
-  gdk-iphonesim-arm64/lib/iphonesimulator/libgreenaddress_full.a \
-  -output libgreenaddress_full.a
+  ${INTEL_FOLDER}/lib/${INTEL_NESTED_FOLDER}/${LIBGREEN}.a \
+  ${SIM_FOLDER}/lib/${SIM_NESTED_FOLDER}/${LIBGREEN}.a \
+  -output ${LIBGREEN}.a
 
 # pack simulators binaries and physical device libraries into the framework
 xcodebuild -create-xcframework \
-  -library gdk-iphone/lib/iphoneos/libgreenaddress_full.a \
-  -library libgreenaddress_full.a \
-  -output libgreenaddress_full.xcframework
+  -library ${DEVICE_FOLDER}/lib/${DEVICE_NESTED_FOLDER}/${LIBGREEN}.a \
+  -library ${LIBGREEN}.a \
+  -output ${LIBGREEN}.xcframework
 
-# # Clean up any previous install
-rm -f gdk-iphone.tar.gz gdk-iphone-sim.tar.gz gdk-iphone-sim-x86_64.tar.gz libgreenaddress_full.a
-rm -rf gdk-iphone gdk-iphonesim-arm64 gdk-iphonesim-x86_64
+# Cleanup
+rm -f ${DEVICE_TARBALL} ${SIM_TARBALL} ${INTEL_TARBALL} ${LIBGREEN}.a
+rm -rf ${DEVICE_FOLDER} ${SIM_FOLDER} ${INTEL_FOLDER}
